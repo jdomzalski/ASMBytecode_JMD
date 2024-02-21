79e4d7253808b8b7a151b8a736010fffab84ccf3
jdomzalski <100503451+jdomzalski@users.noreply.github.com>
2024-02-21 11:13:58 -0500

    Add files via upload

---
+++ gen5.java
@@ -0,0 +1,65 @@
+import java.io.*;
+
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+public class gen5{
+
+    public static void main(String[] args){
+        // Creating the ClassWriter
+        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        cw.visit(Opcodes.V1_8, Opcodes.ACC_PUBLIC,"Strings",null,"java/lang/Object",null);
+
+        {
+            // Created the MethodVisitor for the initial method
+            MethodVisitor mv = cw.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null);
+            mv.visitCode();
+            mv.visitVarInsn(Opcodes.ALOAD, 0); //loading this variable onto the stack
+            mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
+            mv.visitInsn(Opcodes.RETURN);
+            mv.visitMaxs(1,1);
+            mv.visitEnd();
+        }
+
+        {
+            // Creating the MethodVisitor for the main method
+            MethodVisitor mv = cw.visitMethod(Opcodes.ACC_PUBLIC+Opcodes.ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
+            mv.visitCode();
+            // Creating, storing, and loading two string variables
+            mv.visitLdcInsn("This is the first string variable, ");
+            mv.visitVarInsn(Opcodes.ASTORE, 0);
+            mv.visitLdcInsn("And this is the second string variable.")
+            mv.visitVarInsn(Opcodes.ASTORE, 1);
+            mv.visitVarInsn(Opcodes.ALOAD,0);
+            mv.visitVarInsn(Opcodes.ALOAD,1);
+
+            // Printing the two strings that just got loaded from the stack
+            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(D)V", false);
+
+            mv.visitInsn(Opcodes.RETURN);
+            mv.visitMaxs(0,0);
+            mv.visitEnd();
+        }
+
+        cw.visitEnd();
+
+        byte[] b = cw.toByteArray();
+
+        writeFile(b,"program5.class");
+
+        public static void writeFile(byte[] bytearray, String fileName){
+            try{
+                FileOutputStream out = new FileOutputStream(fileName);
+                out.write(bytearray);
+                out.close();
+            }
+            catch(IOException e){
+                System.out.println(e.getMessage());
+            }
+        }
+
+        System.out.println("Done!");
+    } // end main
+
+} // end class
