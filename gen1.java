87f484153f82c259725370ee864c6d99ac375ae1
jdomzalski <100503451+jdomzalski@users.noreply.github.com>
2024-02-22 21:51:12 -0500

    Corrected changes mentioned on Moodle

---
+++ gen1.java
@@ -0,0 +1,112 @@
+import java.io.*;
+
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+public class gen1{
+
+    public static void main(String[] args){
+        // Creating the ClassWriter
+        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        cw.visit(Opcodes.V1_8, Opcodes.ACC_PUBLIC,"program1",null,"java/lang/Object",null);
+
+        {
+            // Created the MethodVisitor for the initial method
+            MethodVisitor mv = cw.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null);
+            mv.visitCode();
+            mv.visitVarInsn(Opcodes.ALOAD, 0); //loading this variable onto the stack
+            mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
+            mv.visitInsn(Opcodes.RETURN);
+            mv.visitMaxs(1,1);
+            mv.visitEnd();
+        }
+
+        {
+            // Creating the MethodVisitor for the main method
+            MethodVisitor mv = cw.visitMethod(Opcodes.ACC_PUBLIC+Opcodes.ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
+            mv.visitCode();
+
+            // Loading and multiplying two ints and storing/printing the result
+            mv.visitLdcInsn(5);
+            mv.visitVarInsn(Opcodes.ISTORE,0);
+            mv.visitLdcInsn(6);
+            mv.visitVarInsn(Opcode.ISTORE,1);
+            mv.visitVarInsn(Opcodes.ILOAD,0);
+            mv.visitVarInsn(Opcodes.ILOAD,1);
+            mv.visitInsn(Opcodes.IMUL);
+            mv.visitVarInsn(Opcodes.ISTORE,2);
+            mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+            mv.visitVarInsn(Opcodes.ILOAD,2);
+            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(D)V", false);
+
+
+            // Loading and multiplying two longs and storing/printing the result
+            mv.visitLdcInsn((long) 25);
+            mv.visitVarInsn(Opcodes.LSTORE,0);
+            mv.visitLdcInsn((long) 500);
+            mv.visitVarInsn(Opcodes.LSTORE,1);
+            mv.visitVarInsn(Opcodes.LLOAD,0);
+            mv.visitVarInsn(Opcodes.LLOAD,1);
+            mv.visitInsn(Opcodes.LMUL);
+            mv.visitVarInsn(Opcodes.LSTORE,2);
+            mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+            mv.visitVarInsn(Opcodes.LLOAD,2);
+            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(D)V", false);
+
+
+            // Loading and multiplying two floats and storing/printing the result
+            mv.visitLdcInsn((float) 3.5);
+            mv.visitVarInsn(Opcodes.FSTORE,0);
+            mv.visitLdcInsn((float) 5.95);
+            mv.visitVarInsn(Opcodes.FSTORE,1);
+            mv.visitVarInsn(Opcodes.FLOAD,0);
+            mv.visitVarInsn(Opcodes.FLOAD,1);
+            mv.visitInsn(Opcodes.FMUL);
+            mv.visitVarInsn(Opcodes.FSTORE,2);
+            mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+            mv.visitVarInsn(Opcodes.FLOAD,2);
+            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(D)V", false);
+
+
+            // Loading and multiplying two doubles and storing/printing the result
+            mv.visitLdcInsn((double) 200.53);
+            mv.visitVarInsn(Opcodes.DSTORE,0);
+            mv.visitLdcInsn((double) 23.23);
+            mv.visitVarInsn(Opcodes.DSTORE,1);
+            mv.visitVarInsn(Opcodes.DLOAD,0);
+            mv.visitVarInsn(Opcodes.DLOAD,1);
+            mv.visitInsn(Opcodes.DMUL);
+            mv.visitVarInsn(Opcodes.DSTORE,2);
+            mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+            mv.visitVarInsn(Opcodes.DLOAD,2);
+            mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(D)V", false);
+
+            mv.visitInsn(Opcodes.RETURN);
+            mv.visitMaxs(0,0);
+            mv.visitEnd();
+
+        }
+
+        cw.visitEnd();
+
+        byte[] b = cw.toByteArray();
+
+        writeFile(b,"program1.class");
+
+        System.out.println("Done!");
+    } // end main
+
+    // Moving writeFile method outside of the main method
+    public static void writeFile(byte[] bytearray, String fileName){
+        try{
+            FileOutputStream out = new FileOutputStream(fileName);
+            out.write(bytearray);
+            out.close();
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+} // end class
---
+++ gen7.java
@@ -0,0 +1,75 @@
+import java.io.*;
+
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+public class gen7 {
+
+    public static void main(String[] args){
+        // Creating the ClassWriter
+        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        cw.visit(Opcodes.V1_8, Opcodes.ACC_PUBLIC,"program7",null,"java/lang/Object",null);
+
+        {
+            // Created the MethodVisitor for the initial method
+            MethodVisitor mv = cw.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null);
+            mv.visitCode();
+            mv.visitVarInsn(Opcodes.ALOAD, 0); //loading this variable onto the stack
+            mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
+            mv.visitInsn(Opcodes.RETURN);
+            mv.visitMaxs(1,1);
+            mv.visitEnd();
+        }
+
+        {
+            // Creating the MethodVisitor for the main method
+            MethodVisitor mv = cw.visitMethod(Opcodes.ACC_PUBLIC+Opcodes.ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
+            mv.visitCode();
+            
+            mv.visitLdcInsn(0);
+            mv.visitVarInsn(Opcodes.ISTORE, 0);
+
+            // Defining the loop
+            LoopCondition startLoop = new LoopCondition();
+            mv.visitLabel(startLoop);
+            mv.visitVarInsn(Opcodes.ILOAD, 0);
+            mv.visitLdcInsn(10);
+            LoopCondition endLoop = new LoopCondition();
+            mv.visitJumpInsn(Opcodes.IFEQ, endLoop); // If stored value equal to 10, the loop will end
+
+
+            // Content of loop
+            mv.visitVarInsn(Opcodes.ILOAD, 0); // loading the value at index 0 and adding 1 to it. This is how we go to the next step in the loop
+            mv.visitIns(Opcodes.ICONST_1); // decalring a constant value of 1 that will be added to the variable at position 0
+            mv.visitInsn(Opcodes.IADD);
+            mv.visitVarInsn(Opcodes.ISTORE, 0); // Storing the incremented value
+
+
+            mv.visitInsn(Opcodes.RETURN);
+            mv.visitMaxs(0,0);
+            mv.visitEnd();
+        }
+
+        cw.visitEnd();
+
+        byte[] b = cw.toByteArray();
+
+        writeFile(b,"program7.class");
+
+        System.out.println("Done!");
+    } // end main
+
+    // Moving writeFile method outside of the main method
+    public static void writeFile(byte[] bytearray, String fileName){
+        try{
+            FileOutputStream out = new FileOutputStream(fileName);
+            out.write(bytearray);
+            out.close();
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+} // end class
